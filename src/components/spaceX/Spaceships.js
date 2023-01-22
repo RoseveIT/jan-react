8c724b9a40ebe5e633aaec5a545cbc3a73d9a1ec
RoseveIT <62Bip23_Sorta557_qq>
2023-01-22 16:48:11 +0200

    lesson hw 2 done

--- src/components/spaceX/Spaceships.js
+++ src/components/spaceX/Spaceships.js
@@ -7,9 +7,8 @@ const Spaceships = () => {
     const [spaceships, setSpaceships] = useState([]);
 
     useEffect(()=>{
-        shipService.getShips().then(value => value.data).then(value => ))
+        shipService.getShips().then(value => value.data).then(value => setSpaceships(value.filter(value => value.launch_year !== '2020')))
     },[])
-
     return (
         <div>
             {spaceships.map((spaceship,index)=><Spaceship key={index} spaceship={spaceship}/>)}
