35e7329dc715732e8ec38011e85d26c59160cb94
RoseveIT <62Bip23_Sorta557_qq>
2023-01-28 15:34:14 +0200

    lesson hw 3

--- src/components/CarsForm/CarsForm.js
+++ src/components/CarsForm/CarsForm.js
@@ -1,11 +1,21 @@
 import React from "react";
+import {useForm} from "react-hook-form";
 
 const CarsForm = () => {
-    return (
-        <form>
+    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm({mode: 'all'});
 
+
+    const submit = (data) =>{
+        console.log(data)
+    }
+
+    return (
+        <form onSubmit={handleSubmit(submit)}>
+            <input type="text" placeholder={'brand'} {...register('brand')}/>
+            <input type="text" placeholder={'price'} {...register('price')}/>
+            <input type="text" placeholder={'year'} {...register('year')}/>
+            <button>Save</button>
         </form>
     );
-};
-
+}
 export {CarsForm};
