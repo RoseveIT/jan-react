a9e6c3cfe0b5642df6b8f814b7b805a7246ef5de
RoseveIT <62Bip23_Sorta557_qq>
2023-02-07 21:03:23 +0200

    lesson 5 useReducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import {useReducer} from "react";
+
 const App = () => {
+    const reducer = (state, action) => {
+        switch (action.type){
+            case 'INC1':
+                return {...state, count1: state.count1+1}
+            case 'DEC1':
+                return {...state, count1: state.count1-1}
+            case 'INC2':
+                return {...state, count2: state.count2+1}
+            case 'DEC2':
+                return {...state, count2: state.count2-1}
+            case 'RESET':
+                return {count1: action.payload, count2: action.payload}
+            default:
+                return {...state}
+        }
+    };
+    const initvalue = () => (
+        {count1:0, count2:0}
+    );
+
+    const [state, dispatch] = useReducer(reducer, null, initvalue);
+
     return (
         <div>
-          App
+            <div>Count1:{state.count1}</div>
+            <button onClick={()=>dispatch({type:'INC1'})}>INC</button>
+            <button onClick={()=>dispatch({type:'DEC1'})}>DEC</button>
+            <div>Count2:{state.count2}</div>
+            <button onClick={()=>dispatch({type:'INC2'})}>INC</button>
+            <button onClick={()=>dispatch({type:'DEC2'})}>DEC</button>
+            <hr/>
+            <button onClick={()=>dispatch({type:'RESET', payload:25})}>RESET</button>
         </div>
     );
 };
