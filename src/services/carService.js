0d2603f4c0283b530d2bdbd7949de07cb85feb95
RoseveIT <62Bip23_Sorta557_qq>
2023-03-22 17:17:58 +0200

    lesson 6 homework

--- src/services/carService.js
+++ src/services/carService.js
@@ -1,6 +1,10 @@
-import {urls} from "../config/urls";
+import {axiosService} from "./axiosService";
+import {urls} from "../config";
 
 const carService = {
-    getAll:()=>{carService.getAll(urls.cars.cars)},
-    create:(car, data)=>{carService.create(urls.cars.)}
+    getAll:()=>{axiosService.get(urls.cars.cars)},
+    create:(data)=>{axiosService.post(urls.cars.cars, data)},
+    updateById:(id, data)=>{axiosService.put(urls.cars.getById(id), data)},
+    deleteByid:(id)=>{axiosService.delete(urls.cars.getById(id))}
 }
+
