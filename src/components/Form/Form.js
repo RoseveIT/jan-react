c3b82bf6377641b188aad0f844f9bc7935d18813
RoseveIT <62Bip23_Sorta557_qq>
2023-03-22 20:16:28 +0200

    lesson 6 homework

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,19 @@
 import React from "react";
 import {useForm} from "react-hook-form";
+import {useDispatch} from "react-redux";
+
 import {carValidator} from "../../validator";
+import {carAction} from "../../redux";
 
 const Form = () => {
 
     const {register, handleSubmit, reset, setValue, formState:{isValid}} = useForm({mode:'all', resolver:carValidator});
 
-    const save = (car) => {
+    const dispatch = useDispatch();
 
+    const save = async (car) => {
+        await dispatch(carAction.create({car}))
+        reset()
     }
 
     return (
@@ -15,7 +21,7 @@ const Form = () => {
             <input type='text' placeholder={'brand'} {...register('brand')}/>
             <input type='text' placeholder={'price'} {...register('price', {valueAsNumber:true})}/>
             <input type='text' placeholder={'year'} {...register('year', {valueAsNumber:true})}/>
-            <button disabled={!isValid}>save</button>
+            <button disabled={!isValid}>create</button>
         </form>
     );
 };
