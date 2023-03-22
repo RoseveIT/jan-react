31686f071b261adaffd891a784687ba3a4e63e74
RoseveIT <62Bip23_Sorta557_qq>
2023-03-22 19:51:40 +0200

    lesson 6 homework

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,11 +6,23 @@ const Form = () => {
 
     const {register, handleSubmit, reset, setValue, formState:{isValid}} = useForm({mode:'all', resolver:carValidator});
 
+    function save() {
+
+    }
+
     return (
-        <form>
-            <input  />
+        <form onSubmit={handleSubmit(save)}>
+            <input type='text' placeholder={'brand'} {...register('brand')}/>
+            <input type='number' placeholder={'price'} {...register('price', {valueAsNumber:true})}/>
+            <input type='number' placeholder={'year'} {...register('year', {valueAsNumber:true})}/>
+            <button disabled={!isValid}>save</button>
         </form>
     );
 };
 
 export {Form};
+
+// <input type="text" placeholder={'brand'} {...register('brand')}/>
+// <input type="text" placeholder={'price'} {...register('price', {valueAsNumber:true})}/>
+// <input type="text" placeholder={'year'} {...register('year', {valueAsNumber:true})}/>
+// <button disabled={!isValid}>{carForUpdate?'update':'create'}</button>
